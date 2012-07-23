d02f62c4c048b8bedeb56f5836b7f20d7b686f6c
Christophe Hamerling <christophe.hamerling@gmail.com>
2012-07-23 11:55:23 +0200

    Add minimal keys

---
+++ play-serviceregistry-api/src/main/java/org/ow2/play/service/registry/api/Constants.java
@@ -0,0 +1,42 @@
+/**
+ *
+ * Copyright (c) 2012, PetalsLink
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA 
+ *
+ */
+package org.ow2.play.service.registry.api;
+
+/**
+ * Defines a minimal list of constants required by the Play platform components.
+ * 
+ * @author chamerling
+ * 
+ */
+public interface Constants {
+
+	public static final String METADATA = "org.ow2.play.metadata";
+
+	public static final String DSB_TO_EC_EC = "org.ow2.play.dsb2ec.eventcloud";
+
+	public static final String DSB_TO_EC_EC_SUBSCRIBER = "org.ow2.play.dsb2ec.subscriber";
+
+	public static final String EC_TO_DSB_DSB = "org.ow2.play.ec2dsb.dsb";
+
+	public static final String EC_TO_DSB_EC = "org.ow2.play.ec2dsb.eventcloud";
+
+	public static final String TOPIC = "org.ow2.play.topic";
+
+}
