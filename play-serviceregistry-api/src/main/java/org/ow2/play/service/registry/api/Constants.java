45a7915cd387487ec883f81b75cb57625a1ade46
Christophe Hamerling <christophe.hamerling@gmail.com>
2012-08-29 16:38:53 +0200

    add constant for subscription service

--- play-serviceregistry-api/src/main/java/org/ow2/play/service/registry/api/Constants.java
+++ play-serviceregistry-api/src/main/java/org/ow2/play/service/registry/api/Constants.java
@@ -43,4 +43,5 @@ public interface Constants {
 	
 	public static final String GOVERNANCE_SUBSCRIPTION_REGISTRY = "org.ow2.play.governance.subscription.registry";
 
+	public static final String GOVERNANCE_SUBSCRIPTION_SERVICE = "org.ow2.play.governance.subscription.service";
 }
